5559952ce2ccc8b826ffb8cab4e93102bdf29def
Salvatore Cozzubo <salvatore.cozzubo@gmail.com>
2021-01-03 21:32:29 +0100

    fix: name class 'PlayerMovement'

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
     private Vector2 movePlayer;

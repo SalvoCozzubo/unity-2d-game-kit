256814bbb0afcc97d84f3838ae4deb66cd934021
Salvatore Cozzubo <salvatore.cozzubo@gmail.com>
2021-01-05 22:10:55 +0100

    npc patrol: smooth movement

--- NPCPatrol.cs
+++ NPCPatrol.cs
@@ -14,35 +14,55 @@ public class NPCPatrol : MonoBehaviour
 
     [SerializeField]
     private Transform[] waypoints;
+
+    [SerializeField]
+    private float waitTime = 2f;
+
+    [SerializeField]
+    private float range = .2f;
+    private float nextChangeWaypoint;
     private int currentWaypointIndex = 0;
     private Vector2 move;
+
+    private float currentSpeed = 0;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        nextChangeWaypoint = waitTime;
+        currentSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (waypoints.Length == 0) return;
         move = waypoints[currentWaypointIndex].position - transform.position;
 
-        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < .2f) {
-            currentWaypointIndex += 1;
+        if (Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position) < range)
+        {
+            currentSpeed = 0;
+            nextChangeWaypoint -= Time.deltaTime;
 
-            if (currentWaypointIndex > waypoints.Length - 1) {
-                currentWaypointIndex = 0;
+            if (nextChangeWaypoint <= 0) {
+                nextChangeWaypoint = waitTime;
+                currentWaypointIndex += 1;
+                currentSpeed = speed;
+
+                if (currentWaypointIndex > waypoints.Length - 1) {
+                    currentWaypointIndex = 0;
+                }
             }
         }
 
+        move.Normalize();
+
         animator.SetFloat("PosX", move.x);
         animator.SetFloat("PosY", move.y);
-        animator.SetFloat("Speed", move.sqrMagnitude);
-    }
+        animator.SetFloat("Speed", currentSpeed);
 
-    void LateUpdate()
-    {
-        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+        rb.velocity = move * currentSpeed;
     }
 }

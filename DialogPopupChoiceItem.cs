825f5c1747c9279b32b6b5a8ca6bd9f741ae8b93
Salvatore Cozzubo <salvatore.cozzubo@gmail.com>
2021-01-11 23:06:42 +0100

    add dialog item

---
+++ DialogPopupChoiceItem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogPopupChoiceItem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject selectIcon;
+    [SerializeField]
+    private TMPro.TMP_Text selectText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetupItem(string text)
+    {
+        selectText.text = text;
+    }
+
+    public void SelectItem()
+    {
+        selectIcon.SetActive(true);
+    }
+
+    public void UnselectItem()
+    {
+        selectIcon.SetActive(false);
+    }
+}

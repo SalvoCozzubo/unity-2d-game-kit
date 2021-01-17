c27e004398803f45156f6cbd7da73eaf4c3a1e1a
Salvatore Cozzubo <salvatore.cozzubo@gmail.com>
2021-01-17 19:11:11 +0100

    update InputManager

--- Scripts/InputManager.cs
+++ Scripts/InputManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Direction { UP, DOWN, LEFT, RIGHT, NONE };
 public class InputManager : MonoBehaviour
 {
+    public static InputManager instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
@@ -28,6 +30,19 @@ public class InputManager : MonoBehaviour
 
     public bool GetActionButton()
     {
-        return Input.GetButton("Action");
+        return Input.GetButtonDown("Action");
+    }
+
+    public Direction GetDirection()
+    {
+        float horizontalAxis = GetHorizontalAxis();
+        if (horizontalAxis > 0) return Direction.LEFT;
+        else if (horizontalAxis < 0) return Direction.RIGHT;
+
+        float verticalAxis = GetVerticalAxis();
+        if (verticalAxis > 0) return Direction.UP;
+        else if (verticalAxis < 0) return Direction.DOWN;
+
+        return Direction.NONE;
     }
 }

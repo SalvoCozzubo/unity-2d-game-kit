521a4f302a4de05354531e8b4193cc9f14e17df4
Salvatore Cozzubo <salvatore.cozzubo@gmail.com>
2021-01-22 23:03:15 +0100

    update PlayerMovement

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -2,49 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Animator))]
-public class PlayerMovement : MonoBehaviour
-{
-    [SerializeField]
-    private InputManager input;
-    private Animator animator;
-    private Rigidbody2D rb;
-    private Vector2 movePlayer;
-
-    [Header("Movement")]
-    [SerializeField]
-    private float speed = 2f;
-
-    // Start is called before the first frame update
-    void Start()
+namespace UnityTools2D {
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Animator))]
+    public class PlayerMovement : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        movePlayer = Vector2.zero;
-    }
+        private Animator animator;
+        private Rigidbody2D rb;
+        private Vector2 movePlayer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
+        [Header("Movement")]
+        [SerializeField]
+        private float speed = 2f;
 
-    void LateUpdate()
-    {
-        rb.MovePosition(rb.position + movePlayer * speed * Time.fixedDeltaTime);
-    }
-
-    void Move()
-    {
-        movePlayer.y = input.GetVerticalAxis();
-        movePlayer.x = input.GetHorizontalAxis();
-
-        if (movePlayer != Vector2.zero) {
-            animator.SetFloat("PosX", movePlayer.x);
-            animator.SetFloat("PosY", movePlayer.y);
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            animator = GetComponent<Animator>();
+            movePlayer = Vector2.zero;
         }
 
-        animator.SetFloat("Speed", movePlayer.sqrMagnitude);
+        // Update is called once per frame
+        void Update()
+        {
+            Move();
+        }
+
+        void LateUpdate()
+        {
+            rb.MovePosition(rb.position + movePlayer * speed * Time.fixedDeltaTime);
+        }
+
+        void Move()
+        {
+            movePlayer.y = InputManager.instance.GetVerticalAxis();
+            movePlayer.x = InputManager.instance.GetHorizontalAxis();
+
+            if (movePlayer != Vector2.zero) {
+                animator.SetFloat("PosX", movePlayer.x);
+                animator.SetFloat("PosY", movePlayer.y);
+            }
+
+            animator.SetFloat("Speed", movePlayer.sqrMagnitude);
+        }
     }
 }

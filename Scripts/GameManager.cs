5bd673673cad55ba9fbdbaf8741a9bc35dfb1771
Salvatore Cozzubo <salvatore.cozzubo@gmail.com>
2021-02-04 21:44:00 +0100

    update GameManager ed InputManager

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -2,66 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
-    private bool gamePause = false;
+namespace UnityTools2D {
 
-    public static GameManager instance;
+    public class GameManager : MonoBehaviour
+    {
+        private bool gamePause = false;
 
-    private bool menuShow = false;
-    [Header("References: Menu")]
-    [SerializeField]
-    private GameObject menuRef;
-    [SerializeField]
-    private GameObject dialogRef;
-    void Awake()
-    {
-        instance = this;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        public static GameManager instance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (InputManager.instance.GetExitMenu())
+        private bool menuShow = false;
+        [Header("References: Menu")]
+        [SerializeField]
+        private GameObject menuRef;
+        [SerializeField]
+        private GameObject dialogRef;
+        void Awake()
         {
-            ShowMenu();
+            instance = this;
         }
-    }
-
-    public bool IsGamePause()
-    {
-        return gamePause;
-    }
-
-    public void Pause()
-    {
-        gamePause = true;
-    }
-
-    public void Resume()
-    {
-        gamePause = false;
-    }
-
-    public void ShowMenu()
-    {
-        menuShow = !menuShow;
-        if(menuShow)
+        // Start is called before the first frame update
+        void Start()
         {
-            Pause();
-        }
-        else {
-            Resume();
+            
         }
 
-        if (menuRef != null)
+        // Update is called once per frame
+        void Update()
         {
-            menuRef.SetActive(menuShow);
+            if (InputManager.instance.GetExitMenu())
+            {
+                ShowMenu();
+            }
+        }
+
+        public bool IsGamePause()
+        {
+            return gamePause;
+        }
+
+        public void Pause()
+        {
+            gamePause = true;
+        }
+
+        public void Resume()
+        {
+            gamePause = false;
+        }
+
+        public void ShowMenu()
+        {
+            menuShow = !menuShow;
+            if(menuShow)
+            {
+                Pause();
+            }
+            else {
+                Resume();
+            }
+
+            if (menuRef != null)
+            {
+                menuRef.SetActive(menuShow);
+            }
         }
     }
 }
--- Scripts/InputManager.cs
+++ Scripts/InputManager.cs
@@ -2,52 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Direction { UP, DOWN, LEFT, RIGHT, NONE };
-public class InputManager : MonoBehaviour
-{
-    public static InputManager instance;
-    // Start is called before the first frame update
-    void Start()
+namespace UnityTools2D {
+    public enum Direction { UP, DOWN, LEFT, RIGHT, NONE };
+    public class InputManager : MonoBehaviour
     {
-        instance = this;
-    }
+        public static InputManager instance;
+        // Start is called before the first frame update
+        void Start()
+        {
+            instance = this;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
 
-    public float GetHorizontalAxis()
-    {
-        return Input.GetAxisRaw("Horizontal");
-    }
+        public float GetHorizontalAxis()
+        {
+            return Input.GetAxisRaw("Horizontal");
+        }
 
-    public float GetVerticalAxis()
-    {
-        return Input.GetAxisRaw("Vertical");
-    }
+        public float GetVerticalAxis()
+        {
+            return Input.GetAxisRaw("Vertical");
+        }
 
-    public bool GetActionButton()
-    {
-        return Input.GetButtonUp("Action");
-    }
+        public bool GetActionButton()
+        {
+            return Input.GetButtonUp("Action");
+        }
 
-    public Direction GetDirection()
-    {
-        float horizontalAxis = GetHorizontalAxis();
-        if (horizontalAxis > 0) return Direction.LEFT;
-        else if (horizontalAxis < 0) return Direction.RIGHT;
+        public bool GetActionAttack()
+        {
+            return Input.GetButtonDown("Fire1");
+        }
 
-        float verticalAxis = GetVerticalAxis();
-        if (verticalAxis > 0) return Direction.UP;
-        else if (verticalAxis < 0) return Direction.DOWN;
+        public Direction GetDirection()
+        {
+            float horizontalAxis = GetHorizontalAxis();
+            if (horizontalAxis > 0) return Direction.LEFT;
+            else if (horizontalAxis < 0) return Direction.RIGHT;
 
-        return Direction.NONE;
-    }
+            float verticalAxis = GetVerticalAxis();
+            if (verticalAxis > 0) return Direction.UP;
+            else if (verticalAxis < 0) return Direction.DOWN;
 
-    public bool GetExitMenu()
-    {
-        return Input.GetButtonUp("Cancel");
+            return Direction.NONE;
+        }
+
+        public bool GetExitMenu()
+        {
+            return Input.GetButtonUp("Cancel");
+        }
     }
 }
